9c4c5a7429eacd37f3b5190278ac7d715c504dd1
John Nicholas <themrtortoise@gmail.com>
2018-03-11 19:45:44 +0000

    moving stuff

--- TestProject1/ConsoleApp1/Program.cs
+++ TestProject1/ConsoleApp1/Program.cs
@@ -6,28 +6,19 @@ using System.Threading.Tasks;
 
 namespace GildedRose
 {
-    public interface IUpdateItem
-    {
-        void UpdateItem();
-    }
-
     public class Program
     {
-        public const string StandardItem = "Standard Item";
-        public const string Salad = "Salad";
-
-
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
             var ItemsInStore = new List<IUpdateItem>
             {
-                new GenericItem(StandardItem, 10, 20),
+                new GenericItem("Standard Item", 10, 20),
                 new FineWine(2, 0),
                 new TinnedFood(0, 80),
                 new Fruit(15, 20),
-                new GenericItem(Salad, 3, 6)
+                new GenericItem("Salad", 3, 6)
             };
 
             var app = new Program(ItemsInStore);
